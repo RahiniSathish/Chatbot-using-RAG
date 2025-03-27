5c7c71caa4cbaac51b6be0651a1dfb456ca7f544
Rahini_Sathish <141403412+RahiniSathish@users.noreply.github.com>
2025-03-27 18:40:26 +0530

    Update app

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-# app.py
+
 import os
 import requests
 import streamlit as st
 from dotenv import load_dotenv
-
 load_dotenv()
-
 server_ip = os.getenv("SERVER_IP_ADDRESS", "localhost")
 server_port = os.getenv("SERVER_PORT_NUMBER", "8000")
 server_url = f"http://{server_ip}:{server_port}"
 
 st.set_page_config(page_title="ChatBot")
-st.title("📄 RAG & LLM Chatbot")
-
+st.title("RAG & LLM Chatbot")
 uploaded_file = st.file_uploader("Upload a text document for indexing", type=["pdf"])
 
 if uploaded_file:
